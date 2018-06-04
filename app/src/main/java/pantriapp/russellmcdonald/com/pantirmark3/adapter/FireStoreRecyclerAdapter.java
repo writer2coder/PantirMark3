00bfce0587634e73eab2062b70f2b15ac0c77939
russMcD <russellmcdonald88@gmail.com>
2018-06-03 23:13:38 -0600

    FirebaseAuth, Firestore db integration. Only need to set up ui and rest of the crud methods.

--- app/src/main/java/pantriapp/russellmcdonald/com/pantirmark3/adapter/FireStoreRecyclerAdapter.java
+++ app/src/main/java/pantriapp/russellmcdonald/com/pantirmark3/adapter/FireStoreRecyclerAdapter.java
@@ -3,6 +3,7 @@ package pantriapp.russellmcdonald.com.pantirmark3.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.EditText;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 
 import pantriapp.russellmcdonald.com.pantirmark3.R;
@@ -26,12 +27,17 @@ public class FireStoreRecyclerAdapter extends RecyclerView.Adapter<FireStoreRecy
     @NonNull
     @Override
     public FireStoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater;
+        inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.cv_pantri_storage, parent, false);
+        return new FireStoreViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FireStoreRecyclerAdapter.FireStoreViewHolder holder, int position) {
-
+        holder.itemName.setText(pantriStorage.get(position).getItem());
+        holder.count.setText(pantriStorage.get(position).getItemCount());
+        holder.amountType.setText(pantriStorage.get(position).getItemCount());
     }
 
     @Override
@@ -56,9 +62,9 @@ public class FireStoreRecyclerAdapter extends RecyclerView.Adapter<FireStoreRecy
 
         FireStoreViewHolder(View itemView){
             super(itemView);
-            itemName = itemView.findViewById(R.id.);
-            amountType= itemView.findViewById(R.id.);
-            count= itemView.findViewById(R.id.);
+            itemName = itemView.findViewById(R.id.item_name);
+            amountType= itemView.findViewById(R.id.amount_view);
+            count= itemView.findViewById(R.id.count);
         }
 
     }
